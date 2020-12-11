7e3a2606b378937bfe9d6a6acd8efe37cf2150e3
SabrinaM2 <73124761+SabrinaM2@users.noreply.github.com>
2020-12-11 22:25:21 +0100

    Update Decompression.h
    Add prototype

--- Decompression.h
+++ Decompression.h
@@ -1,1 +1,8 @@
-#pragma once
+#ifndef DECOMPRESSION_H_INCLUDED
+#define DECOMPRESSION_H_INCLUDED
+
+#include "Compression.h"
+
+decompress_file(file_decompress,file_compressed,dictionary);
+
+#endif // DECOMPRESSION_H_INCLUDED
