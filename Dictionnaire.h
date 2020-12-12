13a64962852af78efcdf0e9f7844b0d23bd42e79
SabrinaM2 <73124761+SabrinaM2@users.noreply.github.com>
2020-12-12 09:12:23 +0100

    Update Dictionnaire.h

--- Dictionnaire.h
+++ Dictionnaire.h
@@ -1,1 +1,13 @@
+#ifndef DICTIONNAIRE_H_INCLUDED
+#define DICTIONNAIRE_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "ARBRE.H"
+
+void storage_letter(FILE* dico,Node* mytree,char* path);
+void Storage_Dictionary(Node* mytree,char* Dico_name);
+
+#endif
